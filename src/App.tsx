80efb798a473f6a2c0ad673d6adcb290f18171e4
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-03-19 13:52:43 +0700

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <main className="overflow-hidden">
+      <Header />
     </main>
   );
 };
