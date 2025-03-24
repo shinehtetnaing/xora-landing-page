92b2c578bd3cf58ca0ec1273ce98e32bd17ca15b
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-03-24 13:58:21 +0700

    pricing section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
