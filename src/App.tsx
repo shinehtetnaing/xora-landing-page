fdb29e4b4a52af2c3539ba5e6e3d984d9248388d
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-03-25 12:18:01 +0700

    testimonials section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 };
