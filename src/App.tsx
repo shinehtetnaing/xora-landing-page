2d0080129e10d0c624ae53a3077bc9c8e8fd9edb
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-03-25 13:54:03 +0700

    footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Download from "./components/Download";
 import Faq from "./components/Faq";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
@@ -16,6 +17,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   );
 };
